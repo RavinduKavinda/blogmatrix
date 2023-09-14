48602a1baa79d1a3efdcef0bc806509e886ca147
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-15 01:03:39 +0530

    assets added

---
+++ frontend/src/assets/data/data.jsx
@@ -0,0 +1,153 @@
+/*-----Navbar-----*/
+export const nav = [
+    {
+      id: 1,
+      text: "home",
+      url: "/",
+    },
+    {
+      id: 2,
+      text: "about",
+      url: "/about",
+    },
+    {
+      id: 3,
+      text: "pages",
+      url: "/pages",
+    },
+    {
+      id: 4,
+      text: "blog",
+      url: "/blog",
+    },
+    {
+      id: 5,
+      text: "contact",
+      url: "/contact",
+    },
+]
+
+/*-----Category-----*/
+export const category = [
+{
+    id: 1,
+    category: "Travel",
+    title: "Adventure Awaits",
+    cover: "../images/category/blog.png",
+  },  
+  {
+    id: 2,
+    category: "Food",
+    title: "Culinary Delights",
+    cover: "../images/category/blog.png",
+  },  
+  {
+    id: 3,
+    category: "Fitness",
+    title: "Healthy Lifestyle",
+    cover: "../images/category/blog.png",
+  },  
+  {
+    id: 4,
+    category: "Technology",
+    title: "Innovation in Progress",
+    cover: "../images/category/blog.png",
+  },  
+  {
+    id: 5,
+    category: "Music",
+    title: "Harmony and Melodies",
+    cover: "../images/category/blog.png",
+  },  
+  {
+    id: 6,
+    category: "Art",
+    title: "Creativity Unleashed",
+    cover: "../images/category/blog.png",
+  }
+] 
+
+/*-----blog-----*/
+export const blog = [
+    {
+        id: 1,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 2,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 3,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 4,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 5,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 6,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 7,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 8,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 9,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+    {
+        id: 10,
+        title: "POST NO.2 - EXPLORING THE WILDERNESS",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo eu justo posuere hendrerit. Integer convallis justo at purus vehicula, in vestibulum nisl vulputate. Etiam pellentesque malesuada efficitur. Sed ac elit vel arcu tincidunt malesuada nec at nulla. Fusce ullamcorper urna a risus dapibus varius. Nunc ut justo vitae justo aliquam iaculis.",
+        category: "Travel",
+        cover: "../images/blogs/blog.png",
+        date: "Sep 15, 2023",
+    },
+]
+
