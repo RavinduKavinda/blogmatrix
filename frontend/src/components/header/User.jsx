b7320121dd4e8d3917978401d0a63e70165fee10
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-15 18:15:14 +0530

    user profile nav

--- frontend/src/components/header/User.jsx
+++ frontend/src/components/header/User.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profile from './../../assets/images/profile.png'
+import { Link } from 'react-router-dom'
+import { RiAccountCircleLine } from 'react-icons/ri'
+import { IoCreateOutline, IoBagCheckOutline } from 'react-icons/io5'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { FiHelpCircle } from 'react-icons/fi'
+import { BiLogOutCircle } from 'react-icons/bi'
+
 
 export const User = () => {
   return (
@@ -7,8 +14,56 @@ export const User = () => {
         {/*-----User Profile-----*/}
         <div className="profile">
             <button className="img">
-                <img src={profile} alt="Profile_Image" width='40px' />
+                <img src={profile} alt="Profile_Image" />
             </button>
+
+            <div className="open__profile box__item">
+              {/*dd profile info*/}
+              <Link to='account'>
+                <div className="image">
+                  <div className="img">
+                    <img src={profile} alt="Profile_Image" />
+                  </div>
+                  <div className="text">
+                    <h5>Ravindu Kavinda</h5>
+                    <p>Gampaha, SL</p>
+                  </div>
+                </div>
+              </Link>
+
+              <Link to='/create'>
+                <button className="box ">
+                  <IoCreateOutline  className='icon'/>
+                  <h4>Create Post</h4>
+                </button>
+              </Link>
+
+              <button className="box">
+                <RiAccountCircleLine className='icon'/>
+                <h4>My Account</h4>
+              </button>
+
+              <button className="box">
+                <IoBagCheckOutline className='icon'/>
+                <h4>My Order</h4>
+              </button>
+
+              <button className="box">
+                <AiOutlineHeart className='icon'/>
+                <h4>Wishlist</h4>
+              </button>
+
+              <button className="box">
+                <FiHelpCircle className='icon'/>
+                <h4>Help</h4>
+              </button>
+
+              <button className="box">
+                <BiLogOutCircle className='icon'/>
+                <h4>Log Out</h4>
+              </button>
+
+            </div>
         </div>
     </>
   )
