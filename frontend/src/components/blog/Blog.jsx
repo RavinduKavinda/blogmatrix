036505f7f10e2147ea44b332a5d5529832682d34
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-17 15:05:18 +0530

    blog - title

--- frontend/src/components/blog/Blog.jsx
+++ frontend/src/components/blog/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './blog.css'
 import { blog } from './../../assets/data/data'
 import { BsTag } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 export const Blog = () => {
   return (
@@ -18,6 +19,9 @@ export const Blog = () => {
                             <BsTag className="icon" />
                             <a href="/">{item.category}</a>
                         </div>
+                        <Link to={`/details/${item.id}`} className='link'>
+                            <h3>{item.title}</h3>
+                        </Link>
                     </div>
                 </div>
             ))}
