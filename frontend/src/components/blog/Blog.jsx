669bfddb63f7623f3be7fc6db930eb0b3cae4cd0
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-17 15:20:54 +0530

    blog date comment shares

--- frontend/src/components/blog/Blog.jsx
+++ frontend/src/components/blog/Blog.jsx
@@ -3,6 +3,9 @@ import './blog.css'
 import { blog } from './../../assets/data/data'
 import { BsTag } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+import { GoClock } from 'react-icons/go'
+import { FaRegComment } from 'react-icons/fa'
+import { PiShareFat } from 'react-icons/pi'
 
 export const Blog = () => {
   return (
@@ -10,7 +13,7 @@ export const Blog = () => {
     <section className="blog">
         <div className="container grid">
             {blog.map((item) => (
-                <div className="box boxitem" key={item.id}>
+                <div className="box box__item" key={item.id}>
                     <div className="img">
                         <img src={item.cover} alt="Blog_Cover" />
                     </div>
@@ -22,6 +25,12 @@ export const Blog = () => {
                         <Link to={`/details/${item.id}`} className='link'>
                             <h3>{item.title}</h3>
                         </Link>
+                        <p>{item.desc.slice(0,150)}...</p>
+                        <div className="date">
+                            <GoClock className="icon" /> <label htmlFor="">{item.date}</label>
+                            <FaRegComment className="icon" /> <label htmlFor="">30</label>
+                            <PiShareFat className="icon" /> <label htmlFor="">Share</label>
+                        </div>
                     </div>
                 </div>
             ))}
